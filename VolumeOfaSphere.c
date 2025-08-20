f6d10460abfcfced10e6a0d7f6fbe282701ed261
singhobama665-bit <singhobama665@gmail.com>
2025-08-20 12:34:24 +0530

    Add files via upload

---
+++ VolumeOfaSphere.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+
+    printf("Hello World \n");
+    float pi = 3.14;
+
+    float r = 9;
+
+    float volume = (pi*r*r*r*4)/3;
+
+    printf("%f", volume);
+    return 0;
+}
