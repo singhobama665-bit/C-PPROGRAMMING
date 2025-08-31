4a2ec2c571cdb613ff0abc130f2bbff34f0bc8e1
singhobama665-bit <singhobama665@gmail.com>
2025-09-01 00:27:34 +0530

    Add files via upload

---
+++ SQRT.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+
+    float x;
+    printf("Enter a number : ");
+    scanf("%f", &x);
+
+    float y;
+    y = sqrt(x);
+    printf("Square root of given number is : %f", y);
+
+    return 0;
+}
+
+
